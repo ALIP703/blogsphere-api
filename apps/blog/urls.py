334a73111e0bf27c988d4224c75bf988af569084
muhammed ali <muhammedalip703@gmail.com>
2024-07-14 15:51:44 +0530

    feature(): create blog api created

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blogs, name="blog"),
+    path("", views.blogs),
+    path("create-blog", views.createBlog),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -31,3 +31,35 @@ def blogs(request):
 
     # Return the JSON response with posts and their associated data
     return Response(response)
+
+
+@api_view(["POST"])
+def createBlog(request):
+    try:
+        if not request.user.is_authenticated:
+            return Response(
+                {
+                    "status": 403,
+                    "message": "You are not allowed to access this resource.",
+                    "data": [],
+                }
+            )
+        # Make a copy of request.data and add the author field
+        blog_data = request.data.copy()
+        blog_data["author"] = request.user.id
+
+        serializer = blogSerializer(data=blog_data)
+        if serializer.is_valid():
+            serializer.save()
+        response = {
+            "data": serializer.data,
+            "message": "successfully created blog",
+            "status": 500,
+        }
+    except Exception as e:
+        response = {
+            "data": [],
+            "message": f"An error occurred: {str(e)}",
+            "status": 500,
+        }
+    return Response(response)
