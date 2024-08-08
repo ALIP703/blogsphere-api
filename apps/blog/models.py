c8c97d1469fa002a84bd890f63e9bf90f44206b6
muhammed ali <muhammedalip703@gmail.com>
2024-08-08 10:46:50 +0530

    enhancement(db):created_at and updated_at added in Comments table

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -76,6 +76,8 @@ class Comments(models.Model):
     parent = models.ForeignKey(
         "self", null=True, blank=True, on_delete=models.CASCADE, related_name="replies"
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.message
