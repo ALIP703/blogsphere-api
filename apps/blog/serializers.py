66cc23ba2ac4145b6dbf5903bce59a265c997679
muhammed ali <muhammedalip703@gmail.com>
2024-08-10 15:27:47 +0530

    enhancement():UserSerializer modified

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -17,7 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "profile"]
+        fields = ["id", "username", "first_name", "last_name", "email", "profile"]
 
 
 # Serializer for the Tags model
